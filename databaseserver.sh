fecc1df7ec4bbb810cfb7c5805d6eab4e513e51d
qjatj <skim.12085@gmail.com>
2019-08-31 22:14:44 +1200

    Fixed shell for databaseserver

--- databaseserver.sh
+++ databaseserver.sh
@@ -10,3 +10,11 @@ mysql -u root -ppassword -e "DROP DATABASE test;"
 mysql -u root -ppassword -e "FLUSH PRIVILEGES;"
 sudo service mysqld restart
 sudo chkconfig mysqld on
+
+export MYSQL_PWD='insecure_mysqlroot_pw'
+echo "mysql-server mysql-server/root_password password $MYSQL_PWD" | debconf-set-selections 
+echo "mysql-server mysql-server/root_password_again password $MYSQL_PWD" | debconf-set-selections
+echo "CREATE DATABASE chineseroom;" | mysql
+echo "CREATE USER 'handler'@'%' IDENTIFIED BY 'insecure_db_pw';" | mysql
+
+cat /src/setup-database.sql | mysql -u handler chineseroom
