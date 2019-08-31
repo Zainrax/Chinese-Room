d3515d55ca37633ce4d67bad43fa76a483877d8d
Patrick <patrickb321@gmail.com>
2019-09-01 06:09:53 +1200

    added npm, fixed webserver bugs

--- source/charactersender/source/index.js
+++ source/charactersender/source/index.js
@@ -1,27 +1,16 @@
-console.log("test");
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
-var length = 4
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+const length = 4;
 
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-    var send = []
-    for (i = 0; i < length; i++){
-        send[i] = letters[Math.floor(Math.random()*letters.length)]
-    }
-    res.send("test");
-    // res.send(send);
-  });
+const express = require("express");
+const router = express.Router();
 
+router.listen(3000);
 
-
-
-
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function(req, res){
-//     res.send('reply')
-// })
-
-// app.listen(3000);
+router.get("/", function(req, res, next) {
+  const selectedLetters = [];
+  for (let i = 0; i < length; i++) {
+    selectedLetters.push(letters[Math.floor(Math.random() * letters.length)]);
+  }
+  res.send(selectedLetters);
+  // res.send(send);
+});
